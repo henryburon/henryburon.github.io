317edc59d7f289a6ddf6b884412030aeebea7210
Henry Buron <henryburon34@gmail.com>
2023-12-11 20:16:11 -0500

    Updated pen thief page

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -28,7 +28,7 @@ var documents = [{
     "id": 5,
     "url": "http://localhost:4000/pen_thief/",
     "title": "PincherX 100 Pen Thief",
-    "body": "2023/10/15 - OpenCV, Python, PincherX 100 Overview: This is where I explain how I completed the project. How far can this line go before going on to the second row? "
+    "body": "2023/10/15 - OpenCV, Python, PincherX 100 Overview: Goal: Use the PincherX 100 robot arm to autonomously grab a purple pen. Github: https://github. com/henryburon/pen-thief  Detect Location of the Purple Pen     First, I used the RGB image from an Intel RealSense camera to create an HSV mask that filtered out every color except purple.     Identify Contour and Calculate Centroid     I added contours around the selected pixels and found the 2D coordinate of the centroid of the largest contour, which I assumed to be the pen.     Align the Images     I then aligned the camera’s Depth Map with the RGB Image and found the pen’s 3D coordinates in the camera’s reference frame.     Transform to Robot Frame     I converted these coordinates to be in the robot arm’s frame, given its 90° rotation and fixed offset.     Command the End-Effector     Finally, I used the InterbotixManipulatorXS Python package to move the end-effector and gripper to the desired coordinate and position.    "
     }, {
     "id": 6,
     "url": "http://localhost:4000/RRT/",
